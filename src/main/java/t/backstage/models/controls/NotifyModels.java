362da460e4d99f2eb47c720a9a347a54abad8fd6
zhangj <zhangjin0908@hotmail.com>
2018-08-23 16:50:16 +0800

    添加消息管理

--- src/main/java/t/backstage/models/controls/NotifyModels.java
+++ src/main/java/t/backstage/models/controls/NotifyModels.java
@@ -1,5 +1,6 @@
 package t.backstage.models.controls;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -7,7 +8,9 @@ import java.util.Map;
 
 import com.alibaba.fastjson.JSONObject;
 
+import t.backstage.models.entitys.TBaseNotify;
 import t.backstage.models.entitys.TNotifySubscription;
+import t.backstage.routing.annotations.Post;
 import t.sql.query.Query;
 
 /***
@@ -20,19 +23,44 @@ import t.sql.query.Query;
 public class NotifyModels {
 	@org.springframework.beans.factory.annotation.Autowired
 	private t.sql.SessionFactory sessionFactory;
+	private SimpleDateFormat sdf =new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"); 
 	/**
 	 * 用来获取当前人员的消息提醒数量
 	 * @param j
 	 * @return
 	 */
-	@t.backstage.routing.annotations.Post
 	public Object notifyCount() {
 		String sql = "select count(1) as notifyCount  from t_base_notify  t where t.msgType in :msgType ";
 		Query<Map<String,Object>> tQuery = sessionFactory.getCurrentSession().createQuery(sql,HashMap.class);
 		tQuery.setParameter("msgType",getSubscription());
 		return tQuery.uniqueResult().get(t.backstage.models.context.DBMapKey.notifyCount);
 	}
-	
+	/**
+	 * 获取当前中所有订阅的消息
+	 * @param j
+	 * @return
+	 */
+	@Post
+	public List<Notify> getNotifyAll(JSONObject j){
+		String sql ="select  *  from t_base_notify  t where t.msgType in :msgType  and t.state = 0.00";
+		Query<TBaseNotify> tQuery = sessionFactory.getCurrentSession().createQuery(sql,TBaseNotify.class);
+		tQuery.setParameter("msgType",getSubscription());
+		List<Notify> result = new ArrayList<Notify>();
+		tQuery.list().forEach(tbn->{
+			Notify n = new Notify();
+			n.setTitle(tbn.getTitle());
+			n.setDatetime(sdf.format(tbn.getCreateTime()));
+			n.setId(tbn.getId());
+			n.setDescription(tbn.getContent());
+			n.setType(getMsgTypeToText(tbn.getMsgType()));
+			n.setAvatar(tbn.getAvatar());
+			result.add(n);
+		});
+		return result;
+	}
+	public String getMsgTypeToText(int type) {
+		return type == 0?"通知":type==1?"消息":type==2?"待办":"未知类型["+type+"]";
+	}
 	/**
 	 * 获取当前订阅的信息
 	 * @return
@@ -47,6 +75,60 @@ public class NotifyModels {
 			typeMsg.add(tns.getMsgType());
 		});
 		return typeMsg;
+	}
+}
+/**
+ * 当前返回给前端的消息类型
+ * @author zhangj
+ */
+class Notify{
+	// 通知ID
+	private String id;
+	// 头像地址
+	private String avatar;
+	// 消息标题
+	private String title;
+	// 消息描述
+	private String description;
+	// 时间
+	private String datetime;
+	// 消息类型
+	private String type;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getAvatar() {
+		return avatar;
+	}
+	public void setAvatar(String avatar) {
+		this.avatar = avatar;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getDatetime() {
+		return datetime;
+	}
+	public void setDatetime(String datetime) {
+		this.datetime = datetime;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
 	}
 }
 
---
+++ src/main/java/t/backstage/models/entitys/TBaseNotify.java
@@ -0,0 +1,103 @@
+package t.backstage.models.entitys;
+
+import java.util.Date;
+
+import javax.persistence.Id;
+
+import t.sql.interfaces.DTO;
+
+/***
+ * 获取当前通知的详细信息
+ * @author zhangj
+ * @date 2018年8月23日 下午4:29:27
+ * @email zhangjin0908@hotmail.com
+ */
+public class TBaseNotify implements DTO{
+	private static final long serialVersionUID = 8992876515942947859L;
+	// 用户唯一ID
+	@Id
+	@javax.persistence.Column
+	private String id;
+	// 消息的标题
+	@javax.persistence.Column
+	private String title;
+	// 消息的内容
+	@javax.persistence.Column
+	private String content;
+	// 当前的状态
+	@javax.persistence.Column
+	private double state;
+	// 消息的类型
+	@javax.persistence.Column
+	private int    msgType;
+	// 连接打开的页面
+	@javax.persistence.Column
+	private String linkPath;
+	// 创建时间
+	@javax.persistence.Column
+	private Date   createTime;
+	// 创建人
+	@javax.persistence.Column
+	private String createUser;
+	// 用户头像
+	@javax.persistence.Column
+	private String avatar;
+	
+	public String getAvatar() {
+		return avatar;
+	}
+	public void setAvatar(String avatar) {
+		this.avatar = avatar;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public double getState() {
+		return state;
+	}
+	public void setState(double state) {
+		this.state = state;
+	}
+	public int getMsgType() {
+		return msgType;
+	}
+	public void setMsgType(int msgType) {
+		this.msgType = msgType;
+	}
+	public String getLinkPath() {
+		return linkPath;
+	}
+	public void setLinkPath(String linkPath) {
+		this.linkPath = linkPath;
+	}
+	public Date getCreateTime() {
+		return createTime;
+	}
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+	public String getCreateUser() {
+		return createUser;
+	}
+	public void setCreateUser(String createUser) {
+		this.createUser = createUser;
+	}
+	
+	
+}
+
